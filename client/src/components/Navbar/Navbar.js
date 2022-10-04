90ee5c8d5e51b73345907e83da4fe82066af8993
deepak-1999 <klcdeepak30@gmail.com>
2022-10-04 20:49:04 +0530

    Fix #17 - Change Trendit.com to logo without text in Navbar.js - Add logos with no text

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,12 +2,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo_no_text from '../../Assets/Images/Logo/Trendit-logos_transparent_no_text.png'
+import './Navbar.css'
 
 function BasicExample() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Trendit.com</Navbar.Brand>
+        <Navbar.Brand href="/"><img src={logo_no_text} className="app-logo-no-text" alt="logo_no_text" /></Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
