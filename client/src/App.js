052bee603f6ab1f1de2cd6edab0b4c2b50c7da0a
Najeeb <najeebthangalkp@gmail.com>
2022-10-05 13:41:23 +0530

    feature : Create a contact-us page [fixes #14]

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
-import ThemeProvider from 'react-bootstrap/ThemeProvider'
-import Navbar from './components/Navbar/Navbar'
-import Hero from './components/Hero/Hero'
-import Home from './components/Home/Home'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Footer from './components/Footer/Footer';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ThemeProvider from "react-bootstrap/ThemeProvider";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Home from "./components/Home/Home";
+import ContactUs from "./components/ContactUs/ContactUs";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Home/>
-      <Footer/>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+          </Routes>
+        </div>
+      </Router>
+      <Footer />
     </div>
   );
 }
