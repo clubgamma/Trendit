9208bfac4628a4c94804bb9a824b3d75653baf2c
Ankit-34 <ankitganatra007@gmail.com>
2022-10-19 12:49:12 +0530

    About US page is not Working #30

--- client/src/components/AboutUs/AboutUs.js
+++ client/src/components/AboutUs/AboutUs.js
@@ -1,18 +1,22 @@
 import React from "react";
-import './about.css'
+import './about.css';
+import link from "../../Assets/Images/Doc/ads.png";
 
 function AboutUS(props) { 
   return (
     <div className="pt-5 pb-3">
-      <h3>About Us</h3>
         <div className="about">
-            <div className="des">
-                <h4>Description</h4>
-                <p>This project is about building a website that provides top trending keywords from all over the internet (Google Trends, Twitter, Youtube, FB, Instagram ) using API.</p>
+            <div className="pack">
+              <img src={link} alt="aboutUs" />
             </div>
-            <div className="pur">
-                <h4>Purpose</h4>
+            <div className="outer">
+              <h3>About Us</h3>
+              <div className="des">
+                <p>This project is about building a website that provides top trending keywords from all over the internet (Google Trends, Twitter, Youtube, FB, Instagram ) using API.</p>
+              </div>
+              <div className="pur">
                 <p>Using keywords from Trendit, a content creator can have the right idea on what to create or what keywords they can post to get maximum reach.</p>
+              </div>
             </div>
         </div>
       <div>
